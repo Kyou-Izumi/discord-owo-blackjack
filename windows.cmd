6d4a22a2e5f72ebdc308fbdaded583d8f06c6e14
Eternityyy <thanhlong0975855842@gmail.com>
2024-03-12 12:53:15 +0700

    Update windows.cmd

--- windows.cmd
+++ windows.cmd
@@ -1,4 +1,13 @@
-npm install
-npx tsc
-node .
+@echo off
+
+echo Installing dependencies...
+call npm i
+IF %ERRORLEVEL% NEQ 0 (
+    echo Failed to install dependencies.
+    exit /b 1
+)
+
+echo Starting Tool...
+npm start
+
 pause
